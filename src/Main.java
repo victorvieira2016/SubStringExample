c934c84f6fa93546abb067e3b70588b115c37fe2
Victor Vieira <victor.a.vieira2014@gmail.com>
2025-11-01 12:38:05 -0400

    SubString example

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,9 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String letters = "fadfasdfasdgkasdnkqnjtnqewjngmxifdngnrjwnj";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.printf("Substring from index 20 to end is \"%s\"%n", letters.substring(20));
+        System.out.printf("Substring from index 3 up to, but not including 6 is \"%s\"%n", letters.substring(3,6));
     }
 }
